f2cc2364ed242ea170e10e9490cac78be975d659
georgetm10 <george_1093@hotmail.com>
2016-05-27 14:50:08 -0500

    inicio
    menos fafturas

--- model/cliente.php
+++ model/cliente.php
@@ -10,6 +10,7 @@ class cliente
 	
 	function __construct($nombre,$apellido1,$apellido2,$observaciones)
 	{
+		$this->id=$id;
 		$this->nombre=$nombre;
 		$this->apellido1=$apellido1;
 		$this->apellido2=$apellido2;
